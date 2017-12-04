33850d23c69e47e43b0c14286edc0538023a137a
Jared Showatatek <jareds@stud.ntnu.no>
2017-12-04 14:54:26 +0100

    new graphs

--- Picoscope/calc.py
+++ Picoscope/calc.py
@@ -1,8 +1,8 @@
 #Functions for calculating the energy consumption per fix
 import xlwt
 def calculate_energy_consumption():
-    fix_period=[1,10,60,1800,3600,43200,86400,2592000,15552000]
-    t= fix_period
+    fix_period=[1,10,60,1800,3600,14399,14400,15551700,15552000]
+    t= [60,3600,86400,2592000,31536000]
     v_sleep, T_sleep, v_wake,T_wake,v_acq, T_acq, v_track, T_track = 2*1E-3,0,93.45*1E-3,4,79*1E-3,0,72*1E-3,1
 
     P_sleep = v_sleep*v_sleep
@@ -15,25 +15,26 @@ def calculate_energy_consumption():
     print(P_acq)
     print(P_track)
 
-    columns,rows= 9,9
+    columns,rows= len(fix_period),len(t)
     Energy_consumption = [[0 for x in range(columns)] for y in range(rows)]
+    print(Energy_consumption)
+
 
     i_iterator= 0
     j_iterator= 0
 
     for i in t:
         for j in fix_period:
-            if(j_iterator>8):
+            if(j_iterator>columns-1):
                 j_iterator = 0   
-
-
             if((j<5) or (j>i) ):
+                print(i_iterator,j_iterator)
                 Energy_consumption[i_iterator][j_iterator]= -1
                 j_iterator = j_iterator +1
                 continue
             elif(j<14400):
                 T_acq   = 1
-            elif (j>14400 and j<15552000):
+            elif (j>14399 and j<15552000):
                 T_acq   = 30      
             elif(j == 15552000):
                 T_acq   = 35
@@ -49,7 +50,7 @@ def calculate_energy_consumption():
             j_iterator = j_iterator +1
 
         i_iterator = i_iterator +1
-        if(i_iterator>8):
+        if(i_iterator>rows-1):
             i_iterator = 0  
     print (Energy_consumption)
     book = xlwt.Workbook()
@@ -60,8 +61,8 @@ def calculate_energy_consumption():
     font.bold = True
     style.font = font
     
-    for i in range(9):
-        for j in range(9):
+    for i in range(columns):
+        for j in range(rows):
             sh.write(i,j,Energy_consumption[j][i])
     book.save('dataa.xls')
 
